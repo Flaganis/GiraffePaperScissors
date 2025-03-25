03964f6aa762563a84570788c882e723110be1bb
Flaganis <flaganis1@gmail.com>
2025-03-25 20:38:22 +0300

    Add function getComputerChoice
    Add paragraph to index.html

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log("Hello external giraffe!")
+
+
+
+      
+      
+
+function getComputerChoice(){
+    let randomNumber = Math.floor(Math.random() * 3);
+    console.log(randomNumber);
+    
+    if (randomNumber === 0)
+        computerChoice = "Giraffe"
+    else if (randomNumber === 1)
+        computerChoice = "Paper"
+    else
+        computerChoice = "Scissors"
+    return computerChoice;
+    
+    
+    
+}
+computerChoice = getComputerChoice()
+console.log (computerChoice)
+
+
