3670621db1aab7cfb45508cabb33a5ca5727a167
Flaganis <flaganis1@gmail.com>
2025-03-26 04:17:36 +0300

    Add playRound and playGame fucntion

--- main.js
+++ main.js
@@ -1,23 +1,84 @@
 const log = console.log;
+
 function getComputerChoice(){
-    let randomNumber = Math.floor(Math.random() * 3);
+    let randomNumber = Math.round(Math.random() * 2);
     log(randomNumber);
     
     if (randomNumber == 0)
-        computerChoice = "Giraffe"
+         return "giraffe"
     else if (randomNumber == 1)
-        computerChoice = "Paper"
+        return "paper"
     else
-        computerChoice = "Scissors"
-    return computerChoice;
+        return "scissors"
+    
 }
-computerChoice = getComputerChoice()
-log(computerChoice)
+
+
 
 function getHumanChoice(){
-let humanChoice = window.prompt("Giraffe, Paper or Scissors?");
-return humanChoice;
+    let humanChoice = window.prompt("Giraffe, Paper or Scissors?");
+    return humanChoice.toLowerCase();
+    
 }
 
-humanChoice = getHumanChoice();
-log(humanChoice)
+
+
+function playGame(){
+    
+    let humanScore = 0;
+    let computerScore =0;
+    function playRound(humanChoice, computerChoice) {
+        
+        log(humanChoice,computerChoice)
+        if (humanChoice === computerChoice)
+            return log("Round draw🤝")
+        else if(humanChoice=="giraffe" && computerChoice=="paper"){
+            computerScore++;
+            return log("You lose, Paper wraps Giraffe")
+        }
+        else if(humanChoice=="paper" && computerChoice=="scissors"){
+            computerScore++;
+            return log("You lose, Scissors slices Paper")
+        }
+        else if(humanChoice=="scissors" && computerChoice=="giraffe"){
+            computerScore++;
+            return log("You lose, Giraff eats Scissors")
+        }
+        else {
+            humanScore++;
+            return log("You win, "+humanChoice+" beats "+computerChoice)
+        }
+    }  
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+    playRound(humanSelection,computerSelection)
+    log("Computer score is "+computerScore)
+    log("Your score is "+humanScore)
+
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    playRound(humanSelection,computerSelection)
+    log("Computer score is "+computerScore)
+    log("Your score is "+humanScore)
+
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    playRound(humanSelection,computerSelection)
+    log("Computer score is "+computerScore)
+    log("Your score is "+humanScore)
+    
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    playRound(humanSelection,computerSelection)
+    log("Computer score is "+computerScore)
+    log("Your score is "+humanScore)
+    
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    playRound(humanSelection,computerSelection)
+    log("Computer score is "+computerScore)
+    log("Your score is "+humanScore)
+    
+
+}
+playGame()
